a6a056b90c45c4dd5f471856f50fa3dd8d32be86
kirillkozlovskyi <cucuruza>
2022-04-27 13:46:02 +0300

    create screen page and widget

---
+++ lib/pages/single_stream_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SingleStream extends StatefulWidget {
+  const SingleStream({Key? key}) : super(key: key);
+
+  @override
+  State<SingleStream> createState() => _SingleStreamState();
+}
+
+class _SingleStreamState extends State<SingleStream> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
