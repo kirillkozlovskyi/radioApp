278807a2aa345fd784f97efd839c069d1ef37327
kirillkozlovskyi <cucuruza>
2022-04-27 19:27:55 +0300

    add get Songs request

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -2,7 +2,10 @@ import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 import 'package:radio/constants.dart';
-import 'package:radio/models/stream.model.dart';
+
+import 'models/song.model.dart';
+import 'models/stream.model.dart';
+
 
 class ApiService {
   Future <List<StreamModel>?> getStreams() async {
@@ -12,6 +15,19 @@ class ApiService {
       if (response.statusCode == 200) {
        List<StreamModel> _model = userModelFromJson(response.body);
        return _model;
+      }
+    } catch(e) {
+      log(e.toString());
+    }
+  }
+  
+  Future <List<SongModel>?> getSongsByStreamId(String streamId) async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.songsListByStreamId + streamId + ApiConstants.requestFormat);
+      var response = await http.get(url);
+      if(response.statusCode == 200) {
+        List<SongModel> _model = songModelFromJson(response.body);
+        return _model;
       }
     } catch(e) {
       log(e.toString());
